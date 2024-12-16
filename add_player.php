2af52b73cbb555a047d893b0d8ab68fe30d1ab47
Salma-007 <salmaelallali11@gmail.com>
2024-12-16 23:23:15 +0100

    ajout de dossier for logos and photos

--- add_player.php
+++ add_player.php
@@ -3,34 +3,10 @@
 
     // Get the form data from POST
     $name = $_POST['name_input'];
-    // $photo_url = $_POST['photo_input'];
     $nationality = $_POST['nationalitySelect'];
     $clubName = $_POST['clubSelect']; 
-    //image infos----------------------------
-    
-    if (isset($_FILES["logo_club"])) {
-        $imageName = $_FILES["logo_club"]["name"];
-        $imageTmpName = $_FILES["logo_club"]["tmp_name"];
-        $imageSize = $_FILES["logo_club"]["size"];
-        $imageError = $_FILES["logo_club"]["error"];
-    } else {
-        echo "Logo du club non téléchargé.";
-        exit;
-    }
-    
-    if (isset($_FILES["photo_input"])) {
-        $imageNamephoto = $_FILES["photo_input"]["name"];
-        $imageTmpNamephoto = $_FILES["photo_input"]["tmp_name"];
-        $imageSizephoto = $_FILES["photo_input"]["size"];
-        $imageErrorphoto = $_FILES["photo_input"]["error"];
-    } else {
-        echo "Photo du joueur non téléchargée.";
-        exit;
-    }
 
-    
-
-    //---------------------------
+    // $logo = $_POST['logo_club'];
     $rating = $_POST['rating_input'];
     $position = $_POST['positionSelect'];
     $pace = $_POST['pace_input'];
@@ -39,6 +15,18 @@
     $shooting = $_POST['shooting_input'];
     $defending = $_POST['defending_input'];
     $physical = $_POST['physical_input'];
+    
+    // photo player upload 
+    $photo = $_FILES['photo_input']['name'];
+    $photo_tmp = $_FILES['photo_input']['tmp_name'];
+    $photo_folder = 'uploads/photos/' . $photo; 
+    move_uploaded_file($photo_tmp, $photo_folder);
+
+    // logo image upload
+    $logo_club = $_FILES['logo_club']['name'];
+    $logo_tmp = $_FILES['logo_club']['tmp_name'];
+    $logo_folder = 'uploads/logos/' . $logo_club; 
+    move_uploaded_file($logo_tmp, $logo_folder);
 
     // fetch nationality id 
     $nationality_id_query = "SELECT id_nationality FROM nationalities WHERE name = ?";
@@ -48,17 +36,6 @@
     $stmt->bind_result($nationality_id);
     $stmt->fetch();
     $stmt->close();
-
-    // fetch club id 
-    // $club_id_query = "SELECT id_club FROM clubs WHERE name = ?";
-    // $stmt = $conn->prepare($club_id_query);
-    // $stmt->bind_param("s", $club);
-    // $stmt->execute();
-    // $stmt->bind_result($club_id);
-    // $stmt->fetch();
-    // $stmt->close();
-
-
 
     // check if the club already exists
     $checkQuery = "SELECT id_club FROM clubs WHERE name = ?";
@@ -72,10 +49,10 @@
         mysqli_stmt_fetch($stmt);
     } 
     else{
-        $imageData = file_get_contents($imageTmpName);
+        // ajout du club s'il n'existe pas
         $insertQuery = "INSERT INTO clubs (name, logo) VALUES (?, ?)";
         $insertStmt = mysqli_prepare($conn, $insertQuery);
-        mysqli_stmt_bind_param($insertStmt, "sb", $clubName, $imageData);
+        mysqli_stmt_bind_param($insertStmt, "ss", $clubName, $logo_club);
         mysqli_stmt_execute($insertStmt);
 
         // Fetch the inserted club's ID
@@ -96,14 +73,13 @@
     echo "Error inserting player statistics: " . $stmt->error;
     exit;
     }
-
     $stmt->close();
-
-    $imageDataphoto = file_get_contents($imageTmpNamephoto);
+    
+    // insertion du joueur
     $insert_player_query = "INSERT INTO players (name_player, photo, id_nationality, id_club, rating, position, id_normal_player) 
     VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($insert_player_query);
-    $stmt->bind_param("ssiiiss", $name, $imageDataphoto, $nationality_id, $club_id, $rating, $position, $normal_player_id);
+    $stmt->bind_param("ssiiiss", $name, $photo, $nationality_id, $club_id, $rating, $position, $normal_player_id);
 
     if ($stmt->execute()) {
         header("Location: index.php");
